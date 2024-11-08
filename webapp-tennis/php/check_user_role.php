7bdfec1b3879aa641ee29570569acd1d0765a749
staccariello <kekko.venditto@gmail.com>
2024-11-08 04:46:23 +0100

    Delete webapp-tennis directory

--- webapp-tennis/php/check_user_role.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['tipo']) && $_SESSION['tipo'] === 'proprietario') {
-    echo 'proprietario';
-} else {
-    echo 'cliente'; // O qualsiasi altro valore tu voglia restituire
-}
-?>
